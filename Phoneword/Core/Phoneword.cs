3f4fd7e78b2c5ecfd8fb388dc575dc7938863efa
Craig Dunn <craig.dunn@conceptdevelopment.net>
2012-08-28 15:39:31 +1000

    [Phoneword] initial commit
    Super-simple cross-platform example

---
+++ Phoneword/Core/Phoneword.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+
+namespace Core
+{
+    public static class PhonewordTranslator
+    {
+        public static string ToNumber(string raw)
+        {
+			if (string.IsNullOrEmpty(raw))
+				return "";
+			else
+				raw = raw.ToUpperInvariant();
+
+			var newNumber = new StringBuilder();
+            foreach (var c in raw)
+            {
+                var result = TranslateToNumber(c);
+                if (result == null)
+                    newNumber.Append(c);
+                else
+                    newNumber.Append(result);
+            }
+            return newNumber.ToString();
+        }
+		static bool Contains (this string keyString, char c)
+		{
+			return keyString.IndexOf(c) >= 0;
+		}
+        static int? TranslateToNumber(char c)
+        {
+            if ("ABC".Contains (c))
+                return 2;
+			else if ("DEF".Contains (c))
+                return 3;
+			else if ("GHI".Contains (c))
+                return 4;
+			else if ("JKL".Contains (c))
+                return 5;
+			else if ("MNO".Contains (c))
+                return 6;
+			else if ("PQRS".Contains (c))
+                return 7;
+			else if ("TUV".Contains (c))
+                return 8;
+			else if ("WXYZ".Contains (c))
+                return 9;
+            return null;
+        }
+    }
+}
+
---
+++ Phoneword/PhonewordAndroid/PhonewordScreen.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Phoneword
+{
+    [Activity(Label = "Phoneword", MainLauncher = true)]
+    public class PhonewordScreen : Activity
+    {
+		string translatedNumber;
+		protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button TranslateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+			Button CallButton = FindViewById<Button>(Resource.Id.CallButton);
+            EditText PhoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+
+			TranslateButton.Click += delegate
+            {
+				translatedNumber = Core.PhonewordTranslator.ToNumber(PhoneNumberText.Text);
+				if (translatedNumber == "") {
+					CallButton.Text = "Call";
+					CallButton.Enabled = false;
+				} else {
+					CallButton.Text = "Call " + translatedNumber;
+					CallButton.Enabled = true;
+					// new AlertDialog.Builder(this).SetMessage("No number entered").SetNeutralButton("Ok", delegate { });
+				}
+            };
+
+			CallButton.Click += delegate {
+				var callIntent = new Intent(Intent.ActionCall);
+				callIntent.SetData(Android.Net.Uri.Parse("tel:" + translatedNumber));
+				StartActivity(callIntent);
+			};
+        }
+    }
+}
