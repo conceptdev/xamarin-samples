64468334a33047edee7e72c36ede1294cf03941e
Craig Dunn <craig.dunn@conceptdevelopment.net>
2012-09-04 17:54:54 +1000

    [Phoneword] Win8 shows a 'history' to use screen real-estate

--- Phoneword/PhonewordWin8/MainPage.xaml.cs
+++ Phoneword/PhonewordWin8/MainPage.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -24,6 +26,12 @@ namespace PhonewordWin8
         public MainPage()
         {
             this.InitializeComponent();
+
+            Translations = new ObservableCollection<PhonewordTranslation>();
+
+            Translations.Add(new PhonewordTranslation() { Phoneword = "1-800-flowers",  Phonenumber = "1-800-3569377" });
+            Translations.Add(new PhonewordTranslation() { Phoneword = "1-800-Business", Phonenumber = "1-800-28746377" });
+            Translations.Add(new PhonewordTranslation() { Phoneword = "1-800-THRIFTY",  Phonenumber = "1-800-8474389" });
         }
 
         /// <summary>
@@ -33,19 +41,32 @@ namespace PhonewordWin8
         /// property is typically used to configure the page.</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
+            ListItems.ItemsSource = Translations;
         }
+
         string translatedNumber;
+        public ObservableCollection<PhonewordTranslation> Translations;
+
         private void Translate_Click(object sender, RoutedEventArgs e)
         {
             if (!String.IsNullOrEmpty(PhoneNumberText.Text))
             {
                 translatedNumber = Core.PhonewordTranslator.ToNumber(PhoneNumberText.Text);
                 CallText.Text = "Call " + translatedNumber;
+
+                Translations.Add(new PhonewordTranslation() {Phoneword=PhoneNumberText.Text, Phonenumber=translatedNumber });
             }
             else
             {
                 CallText.Text = "";
             }
+        }
+
+        private void ListItems_Click(object sender, ItemClickEventArgs e)
+        {
+            var ci = e.ClickedItem as PhonewordTranslation;
+            var dialog = new MessageDialog( String.Format ("{0} translates to {1}", ci.Phoneword, ci.Phonenumber));
+            dialog.ShowAsync();
         }
     }
 }
