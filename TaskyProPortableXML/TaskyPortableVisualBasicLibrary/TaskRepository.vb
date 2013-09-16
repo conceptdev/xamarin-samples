c335cd505ac2b56281ce675b9236b3e46656a6ca
Craig Dunn <craig.dunn@conceptdevelopment.net>
2013-09-16 15:39:40 -0700

    [TaskyPortable] add some comments to the VB project

--- TaskyProPortableXML/TaskyPortableVisualBasicLibrary/TaskRepository.vb
+++ TaskyProPortableXML/TaskyPortableVisualBasicLibrary/TaskRepository.vb
@@ -2,18 +2,28 @@
 ''' <summary>
 ''' Implementation of Task (to-do item) storage
 ''' </summary>
+''' <remarks>
+''' Loads and saves the Task objects as an XML file.
+''' Because Portable Class Libraries cannot reference System.IO we have to 
+''' pass in a class (via Interface) that loads and saves the xml file for us
+''' </remarks>
 Public Class TaskRepository
 
     Private _filename As String
     Private _storage As IXmlStorage
     Private _tasks As List(Of Task)
 
+    ''' <summary>
+    ''' Constructor (for those who know C# better than VB)
+    ''' </summary>
     Public Sub New(filename As String, storage As IXmlStorage)
         _filename = filename
         _storage = storage
         _tasks = _storage.ReadXml(filename)
     End Sub
-
+    ''' <summary>
+    ''' Inefficient search for a Task by ID
+    ''' </summary>
     Public Function GetTask(id As Integer) As Task
         For t As Integer = 0 To _tasks.Count - 1
             If _tasks(t).ID = id Then
@@ -23,12 +33,16 @@ Public Class TaskRepository
         Next
         Return New Task() With {.ID = id}
     End Function
-
+    ''' <summary>
+    ''' List all the Tasks 
+    ''' </summary>
     Public Function GetTasks() As IEnumerable(Of Task)
         Return _tasks
     End Function
-
-
+    ''' <summary>
+    ''' Save a Task to the Xml file
+    ''' Calculates the ID as the max of existing IDs
+    ''' </summary>
     Public Function SaveTask(item As Task) As Integer
         Dim max As Integer = 0
 
@@ -46,8 +60,9 @@ Public Class TaskRepository
         _storage.WriteXml(_tasks, _filename)
         Return max
     End Function
-
-
+    ''' <summary>
+    ''' 
+    ''' </summary>
     Public Function DeleteTask(id As Integer) As Integer
         For t As Integer = 0 To _tasks.Count - 1
             If _tasks(t).ID = id Then
