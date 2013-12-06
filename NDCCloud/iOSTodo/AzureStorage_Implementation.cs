ca619124bd16b28f468507e1c0ea602ace6145df
Craig Dunn <craig.dunn@conceptdevelopment.net>
2013-12-06 05:16:06 -0800

    [NDCCloud] add Parse to PCL app

--- NDCCloud/iOSTodo/AzureStorage_Implementation.cs
+++ NDCCloud/iOSTodo/AzureStorage_Implementation.cs
@@ -142,25 +142,6 @@ namespace iOSTodo
 		}
 
 		#endregion
-
-		public List<TodoItem> ReadXml (string filename)
-		{
-			if (File.Exists (filename)) {
-				var serializer = new XmlSerializer (typeof(List<TodoItem>));
-				using (var stream = new FileStream (filename, FileMode.Open)) {
-					return (List<TodoItem>)serializer.Deserialize (stream);
-				}
-			}
-			return new List<TodoItem> ();
-		}
-
-		public void WriteXml (List<TodoItem> tasks, string filename)
-		{
-			var serializer = new XmlSerializer (typeof(List<TodoItem>));
-			using (var writer = new StreamWriter (filename)) {
-				serializer.Serialize (writer, tasks);
-			}
-		}
 	}
 }
 
